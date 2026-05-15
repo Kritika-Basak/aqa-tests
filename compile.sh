9404c8c4f87123900306e32b4f603ac8377483cc
Kritika-Basak <141907438+Kritika-Basak@users.noreply.github.com>
2026-05-15 14:06:37 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+echo "Skipping compilation as per requirement..."
+exit 0
+
 usage() {
     echo '                Please export USE_TESTENV_PROPERTIES, JDK_VERSION and JDK_IMPL before running the script locally.'
 
